dbc6f276dc8b199064c27f5fa9513e56299bedc9
Feduk <my.main.mail.mops@gmail.com>
2023-11-20 15:14:30 +0200

    v 0.4.2 added OrderController, refactored order repository

--- src/main/kotlin/src/Main.kt
+++ src/main/kotlin/src/Main.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import src.model.*
 import src.repo.OrderRepository
+import java.util.Objects.equals
 
 @SpringBootApplication
 class OnlineShop
--- src/main/kotlin/src/repo/OrderRepository.kt
+++ src/main/kotlin/src/repo/OrderRepository.kt
@@ -4,20 +4,28 @@ import src.model.Order
 import src.model.OrderStatus
 import src.model.User
 import src.DataBase
+import javax.swing.text.StyledEditorKit.BoldAction
 
 class OrderRepository {
-    fun addNewOrder(order: Order) {
-        DataBase.orders.add(order)
-    }
-    fun getUserOrders(user: User): List<Order> {
-        return DataBase.orders.filter { user.login == it.user.login };
+    fun addNewOrder(order: Order):Boolean {
+        return if (DataBase.orders.isNotEmpty()) {
+            val newOrder = Order(DataBase.orders.maxOf { it.id } + 1, order.user, order.products)
+            DataBase.orders.add(newOrder)
+        } else
+            DataBase.orders.add(order)
     }
 
-    fun changeOrderStatus(idOrder: Int, orderStatus: OrderStatus) {
+    fun getUserOrders(userLogin: String): List<Order> {
+        return DataBase.orders.filter { it.user.login == userLogin};
+    }
+
+    fun updateOrderStatus(idOrder: Int, orderStatus: OrderStatus):Boolean {
         for (item in DataBase.orders) {
             if(item.id == idOrder) {
                 item.status = orderStatus
+                return true
             }
         }
+        return false
     }
 }
